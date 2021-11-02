017bb6a8447a8fef87a1e4b5ac44d1295b2e2b84
Alexey Roytman <roytman@il.ibm.com>
2021-11-02 15:11:13 +0200

    change log level to 6
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>

--- install_ovsdb_etcd.sh
+++ install_ovsdb_etcd.sh
@@ -33,7 +33,7 @@ if [[ "${USE_OVSDB_ETCD}" = "yes" ]]; then
 
     pushd $GOPATH/src
     git clone https://github.com/roytman/ovsdb-etcd-1
-    pushd ovsdb-etcd
+    pushd ovsdb-etcd-1
     git checkout timeStamps3
     make build
 
